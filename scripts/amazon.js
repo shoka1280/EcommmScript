f0fce035cd9e5d70f5048c0fa3fcba6e4337c8a6
shre2 <shreunq123@gmail.com>
2025-12-15 17:10:40 +0530

    made function nad moved

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,46 +1,4 @@
-// const product=[
-//   {
-//     image:src='images/products/athletic-cotton-socks-6-pairs.jpg',
-//     name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-//     rating:{
-//       stars:4.5,
-//       count:87
-//     },
-//     priceCents:1090,
 
-              
-//   },
-//    {
-//     image:src='images/products/intermediate-composite-basketball.jpg',
-//     name: 'Intermediate Size Basketball',
-//     rating:{
-//       stars:4,
-//       count:127
-//     },
-//     priceCents:2095,
-
-              
-//   },
-//   {
-//      image:src='images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-//     name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-//     rating:{
-//       stars:4.5,
-//       count:56
-//     },
-//     priceCents:799,
-
-//   },
-//   {
-//      image:src='images/products/black-2-slot-toaster.jpg',
-//     name: '2 Slot toaster-black',
-//     rating:{
-//       stars:5,
-//       count:2197
-//     },
-//     priceCents:18.99,
-//   }
-//   ];//list of product
   import {cart,addToCart } from '../data/cart.js'
   import {products} from '../data/products.js'
   let productHtml=``;
